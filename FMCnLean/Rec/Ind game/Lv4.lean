474f3b1e03d7eb0ac85fb819ef3d3793c5517fdf
https://JJoaoll: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@github.com <joaoduos@gmail.com>
2024-11-21 18:57:06 -0300

    adjusting a bit

--- FMCnLean/Rec/Ind game/Lv4.lean
+++ FMCnLean/Rec/Ind game/Lv4.lean
@@ -1,12 +1,12 @@
 namespace Lv4
 
 inductive List (α : Type) where
-  | nil
-  | cons : α → List α → List α
+  | Nil
+  | Cons : α → List α → List α
   deriving Repr
 
 open List
-infixr:50 " :: " => cons
+infixr:50 " :: " => Cons
 
 section
   -- E agora as ListNat são caso especial
@@ -14,45 +14,29 @@ section
   def ListNat := List Nat
 
   #check ListNat
-  #check 3 :: 2 :: 1 :: nil
+  #check 3 :: 2 :: 1 :: Nil
 end
 
 /- Relações em listas polimórficas -/
 
 -- Algumas funções
 def length : List α → Nat
-  | nil     => 0
+  | Nil     => 0
   | _ :: xs => 1 + (length xs)
 
 def concat : List α → List α → List α
-  | nil, ys => sorry
-  | x :: xs, ys => sorry
+  | Nil, ys     => ys
+  | x :: xs, ys => x :: concat xs ys
 
   infixr:50 " ++ " => concat
 
-
--- Algumas relações
-def SameSize (l₁ l₂ : List α) : Prop :=
-  length l₁ = length l₂
-
+-- 
 def Prefix (l₁ l₂ : List α) : Prop :=
   -- Uma dica: as funções definidas antes
   sorry
 
-
 -- Alguns teoremas sobre nossas relações
 variable (l r s : List α)
-
-section -- SameSize]
-  theorem SameSize_refl : SameSize l l := by
-    sorry
-
-  theorem SameSize_symm : SameSize l r ↔ SameSize r l := by
-    sorry
-
-  theorem SameSize_trans : SameSize l r ∧ SameSize r s → SameSize l s := by
-    sorry
-end
 
 section -- Prefix
   theorem Prefix_refl : /- sorry -/ := by
@@ -64,5 +48,11 @@ section -- Prefix
   theorem Prefix_antisymm : /- sorry -/ := by
     sorry
 end
+
+
+
+--------------MAYBE--------------
+
+
 
 end Lv4
