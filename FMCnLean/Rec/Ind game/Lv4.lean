5255d0a2d85291123b9183521bdd0d13f0a0d3d6
LucasArruda12 <lucas123manoel4@gmail.com>
2024-11-21 14:45:11 -0300

    bonitando o arquivo

--- FMCnLean/Rec/Ind game/Lv4.lean
+++ FMCnLean/Rec/Ind game/Lv4.lean
@@ -10,13 +10,14 @@ infixr:50 " :: " => cons
 
 section
   -- E agora as ListNat são caso especial
-  -- Das nossas novas listas polimórficas
+  -- das nossas novas listas polimórficas
   def ListNat := List Nat
 
+  #check ListNat
   #check 3 :: 2 :: 1 :: nil
 end
 
-section -- Relações em listas polimórficas
+/- Relações em listas polimórficas -/
 
 -- Algumas funções
 def length : List α → Nat
@@ -24,39 +25,44 @@ def length : List α → Nat
   | _ :: xs => 1 + (length xs)
 
 def concat : List α → List α → List α
-  | nil, ys => ys
-  | x :: xs, ys => x :: (concat xs ys)
-infixr:50 " ++ " => concat
+  | nil, ys => sorry
+  | x :: xs, ys => sorry
 
+  infixr:50 " ++ " => concat
+
+
+-- Algumas relações
 def SameSize (l₁ l₂ : List α) : Prop :=
   length l₁ = length l₂
 
--- Algumas relações
+def Prefix (l₁ l₂ : List α) : Prop :=
+  -- Uma dica: as funções definidas antes
+  sorry
+
+
+-- Alguns teoremas sobre nossas relações
 variable (l r s : List α)
 
-theorem SameSize_refl : SameSize l l := by
-  sorry
+section -- SameSize]
+  theorem SameSize_refl : SameSize l l := by
+    sorry
 
-theorem SameSize_symm : SameSize l r ↔ SameSize r l := by
-  sorry
+  theorem SameSize_symm : SameSize l r ↔ SameSize r l := by
+    sorry
 
-theorem SameSize_trans : SameSize l r ∧ SameSize r s → SameSize l s := by
-  sorry
+  theorem SameSize_trans : SameSize l r ∧ SameSize r s → SameSize l s := by
+    sorry
+end
 
--- Sua vez de escrever!
--- Uma dica: eu defini uma concat lá em cima
-def Prefix (l₁ l₂ : List α) : Prop :=
-  sorry
+section -- Prefix
+  theorem Prefix_refl : /- sorry -/ := by
+    sorry
 
-theorem Prefix_refl : /- sorry -/ := by
-  sorry
+  theorem Prefix_trans : /- sorry -/ := by
+    sorry
 
-theorem Prefix_antisymm : /- sorry -/ := by
-  sorry
-
-theorem Prefix_trans : /- sorry -/ := by
-  sorry
-
+  theorem Prefix_antisymm : /- sorry -/ := by
+    sorry
 end
 
 end Lv4
