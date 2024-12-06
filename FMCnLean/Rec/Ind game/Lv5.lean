50ad6d8b87c7419ad62b38cb2a490f61c0f8e04a
JJoaoll <joaoduos@gmail.com>
2024-12-06 11:57:57 -0300

    Funções done

--- FMCnLean/Rec/Ind game/Lv5.lean
+++ FMCnLean/Rec/Ind game/Lv5.lean
@@ -3,47 +3,59 @@ namespace Lv5
  -- Talvez tu precise fazer as listas aqui também :)
  -- Aconselho aproveitar o que já fez até agora. Tá quase lá!
 
-inductive BinTree (α : Type) : Type
-  | Nil  : BinTree α
-  | Node : α → BinTree α → BinTree α → BinTree α
+inductive Tree (α : Type) : Type
+  | Nil  : Tree α
+  | Node : α → Tree α → Tree α → Tree α
   deriving Repr, DecidableEq
 
-open BinTree
+open Tree
 
 -- quantos 'andares' tem a árvore
-def height : BinTree α → Nat
+def height : Tree α → Nat
   | tree  => sorry
 
 -- quanta 'informação' tem na árvore
-def size : BinTree α → Nat
+def size : Tree α → Nat
   | tree  => sorry
 
 -- converte a árvore numa lista
-def flatten : BinTree α → List α
+def flatten : Tree α → List α
   | tree  => sorry
 
-def reverse : BinTree α → BinTree α
+def reverse : Tree α → Tree α
   | tree  => sorry
 
 -- Uma árvore binária é dita cheia (full) sse
 -- ∀ nó, se esse nó tem alguma sub-árvore vazia
 --     , então ele está no último nível.
 
-def isFull : BinTree α → Bool
+def isFull : Tree α → Bool
   | tree  => sorry
 
 -- uma heap max, é uma estrutura onde
 -- cada nó eh maior ou igual a todos os seus filhos.
 -- (A raíz da árvore, por consequência, terá o maior valor)
-def isHeapMax [Ord α] : BinTree α → Bool
+def isHeapMax [Ord α] : Tree α → Bool
   | tree  => sorry
 
--- ideias:
--- countLeaves: conta as folhas
--- contains: o flip de um isIn ou In
--- map
--- filter
--- fold
+-- No nosso caso, uma folha
+-- será um nó sem filhos
+def countLeaves : Tree α → Nat 
+  | tree => sorry
+
+def contains [Eq α] : α → Tree α → Bool
+  | tree => sorry
+
+-- Tambbém dê o tipo das funções abaixo
+/-  def mapTree    :   -/
+/-    | tree => sorry  -/
+/--/
+/- def filterTree : -/
+/-   | tree => sorry -/
+/--/
+/- def foldTree :  -/
+/-   | tree => sorry -/
+
 -- instanciar Lista antiga como funktor
 -- instanciar arvore como funKtor
 
@@ -54,16 +66,16 @@ section -- Funktor!!
     id_map : ∀ (x : f α), map id x = id x
     comp_map : ∀ (g : α → β) (h : β → γ) (x : f α), map (h ∘ g) x = (map h ∘ map g) x
 
-  def map : (α → β) → BinTree α → BinTree β
+  def map : (α → β) → Tree α → Tree β
     | tree => sorry
 
-  def id_map : ∀ (x : BinTree α), map id x = id x := by
+  def id_map : ∀ (x : Tree α), map id x = id x := by
     sorry
 
-  def comp_map : ∀ (f : α → β) (g : β → γ) (x : BinTree α), map (g ∘ f) x = (map g ∘ map f) x := by
+  def comp_map : ∀ (f : α → β) (g : β → γ) (x : Tree α), map (g ∘ f) x = (map g ∘ map f) x := by
     sorry
 
-  instance : Funktor BinTree where
+  instance : Funktor Tree where
     map := map
     id_map := id_map
     comp_map := comp_map
@@ -73,6 +85,7 @@ end section
 -- cuidado, talvez tenha algo errado ;)
 -- [Depois de refutar, tenta acrescentar uma hipótese legal
 --   que torne demonstrável, caso precise :P ]
+
 -- demons:
 -- rev da rev
 -- size = lenght (flatten)
